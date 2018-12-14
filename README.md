f01a7f9f6f314421c3cfbaa323582c746aceb576
Kenton Huang <huangkenton@gmail.com>
2018-12-14 13:27:45 -0800

    react webpack setup and completed api

---
+++ README.md
@@ -0,0 +1,3 @@
+# React and Django Setup Test
+
+Following instructions from [Valentino Gagliardi](https://www.valentinog.com/blog/tutorial-api-django-rest-react/)
--- django-drf-react/project/leads/views.py
+++ django-drf-react/project/leads/views.py
@@ -5,5 +5,5 @@ from rest_framework import generics
 
 # Create your views here.
 class LeadListCreate(generics.ListCreateAPIView):
-  queryset = Lead.objects.all()
+  queryset = Leads.objects.all()
   serializer_class = LeadSerializer
